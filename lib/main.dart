8b80df16dfe53a019d4a305f4b40df4b4f8868b1
Mohamed Maher <m7mdellham77@gmail.com>
2024-09-03 18:49:19 +0300

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
+import 'package:bloc_start/app_router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(RickAndMorty(
+    appRouter: AppRouter(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RickAndMorty extends StatelessWidget {
+  final AppRouter appRouter;
 
-  // This widget is the root of your application.
+  const RickAndMorty({Key? key, required this.appRouter}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: appRouter.generateRoute,
+    );
   }
 }
