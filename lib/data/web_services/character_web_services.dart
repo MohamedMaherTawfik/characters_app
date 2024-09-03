1813c8c3767451d94ba9546f48df5b6afd9e4943
Mohamed Maher <m7mdellham77@gmail.com>
2024-09-03 19:54:27 +0300

    create api calling class using dio

---
+++ lib/data/web_services/character_web_services.dart
@@ -0,0 +1,26 @@
+import 'package:bloc_start/constants/strings.dart';
+import 'package:dio/dio.dart';
+
+class CharactersWebServices {
+  late Dio dio;
+
+  CharactersWebServices() {
+    BaseOptions options = BaseOptions(
+        baseUrl: baseUrl,
+        receiveDataWhenStatusError: true,
+        connectTimeout: Duration(seconds: 20),
+        receiveTimeout: Duration(seconds: 20));
+
+    dio = Dio(options);
+  }
+
+  Future<List<dynamic>> getAllCharacters() async {
+    try {
+      Response response = await dio.get('character');
+      return response.data;
+    } catch (e) {
+      print(e.toString());
+      return [];
+    }
+  }
+}
